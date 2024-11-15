de5c0876378ebc2dc201b172d252d60f8219067b
Vijay Singh <vijaysg@altair.com>
2024-11-16 02:40:56 +0530

    msg

--- src/main.cpp
+++ src/main.cpp
@@ -77,6 +77,7 @@ int main(int argc, char *argv[])
                             {
                                 tokens.push_back("STRING \"" + quote + "\" " + quote);
                                 quote_index = -1;
+                                quote       = "";
                             }
                             else
                             {
