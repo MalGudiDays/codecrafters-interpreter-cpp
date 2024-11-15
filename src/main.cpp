740c4390df291e4739baa593b52b8a650d817516
Vijay Singh <vijaysg@altair.com>
2024-11-16 01:32:37 +0530

    msg

--- src/main.cpp
+++ src/main.cpp
@@ -22,67 +22,77 @@ int main(int argc, char *argv[])
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
 
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    std::cerr << "Logs from your program will appear here!" << std::endl;
-
-    if(argc < 3)
+    try
     {
-        std::cerr << "Usage: ./your_program tokenize <filename>" << std::endl;
-        return 1;
-    }
-
-    const std::string command = argv[1];
-
-    if(command == "tokenize")
-    {
-        std::string file_contents = read_file_contents(argv[2]);
-
-        // Uncomment this block to pass the first stage
-
-        if(!file_contents.empty())
+        std::vector<std::string> args(argv + 1, argv + argc);
+        if(args.size() < 2)
         {
-            std::istringstream          stream(file_contents);
-            std::string                 line;
-            std::vector<std::string>    tokens;
-            std::map<char, std::string> token_map = {{'(', "LEFT_PAREN "},
-                                                     {')', "RIGHT_PAREN "},
-                                                     {'{', "LEFT_BRACE "},
-                                                     {'}', "RIGHT_BRACE "},
-                                                     {',', "COMMA "},
-                                                     {'.', "DOT "},
-                                                     {'-', "MINUS "},
-                                                     {'+', "PLUS "},
-                                                     {';', "SEMICOLON "},
-                                                     {'*', "STAR "},
-                                                     {'/', "SLASH "}};
-            int                         line_num  = 1;
-            while(std::getline(stream, line))
-            {
-                for(char ch: line)
-                {
-                    if(token_map.find(ch) != token_map.end())
-                    {
-                        tokens.push_back(token_map[ch] + ch + " null");
-                    }
-                    else
-                    {
-                        retVal = 65;
-                        tokens.push_back("[line " + std::to_string(line_num) +
-                                         "] Error: Unexpected character: " + ch);
-                    }
-                }
-                ++line_num;
-            }
-            printTokens(tokens);
+            std::cerr << "Usage: ./your_program tokenize <filename>" << std::endl;
+            return 64;
         }
-        std::cout << "EOF  null" << std::endl;
-        // Placeholder, remove this line when
-        // implementing the scanner
+
+        const std::string command = args[0];
+
+        if(command == "tokenize")
+        {
+            std::string file_contents = read_file_contents(args[1]);
+
+            // Uncomment this block to pass the first stage
+
+            if(!file_contents.empty())
+            {
+                std::istringstream          stream(file_contents);
+                std::string                 line;
+                std::vector<std::string>    tokens;
+                std::map<char, std::string> token_map = {{'(', "LEFT_PAREN "},
+                                                         {')', "RIGHT_PAREN "},
+                                                         {'{', "LEFT_BRACE "},
+                                                         {'}', "RIGHT_BRACE "},
+                                                         {',', "COMMA "},
+                                                         {'.', "DOT "},
+                                                         {'-', "MINUS "},
+                                                         {'+', "PLUS "},
+                                                         {';', "SEMICOLON "},
+                                                         {'*', "STAR "},
+                                                         {'/', "SLASH "}};
+                int                         line_num  = 1;
+                while(std::getline(stream, line))
+                {
+                    for(char ch: line)
+                    {
+                        if(token_map.find(ch) != token_map.end())
+                        {
+                            tokens.push_back(token_map[ch] + ch + " null");
+                        }
+                        else
+                        {
+                            retVal = 65;
+                            tokens.push_back("[line " + std::to_string(line_num) +
+                                             "] Error: Unexpected character: " + ch);
+                        }
+                    }
+                    if(retVal != 0)
+                    {
+                        printTokens(tokens);
+                        return retVal;
+                    }
+                    ++line_num;
+                }
+                printTokens(tokens);
+            }
+            std::cout << "EOF  null" << std::endl;
+            // Placeholder, remove this line when
+            // implementing the scanner
+        }
+        else
+        {
+            std::cerr << "Unknown command: " << command << std::endl;
+            return 1;
+        }
     }
-    else
+    catch(const std::exception &e)
     {
-        std::cerr << "Unknown command: " << command << std::endl;
-        return 1;
+        std::cerr << e.what() << '\n';
     }
 
     return retVal;
