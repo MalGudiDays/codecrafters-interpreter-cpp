9c1a6f4503473ffc9ac39f5ebe89a19b91b23b54
Vijay Singh <vijaysg@altair.com>
2024-11-16 09:31:50 +0530

    msg

--- src/tokenize.cpp
+++ src/tokenize.cpp
@@ -70,7 +70,7 @@ void Tokenizer::processCharacter(char ch, int &index, const std::string &line)
     {
         handleToken(ch, index, line);
     }
-    else if(ch == '$' || ch == '#' || ch == '@')
+    else if(ch == '%' || ch == '#' || ch == '@')
     {
         retVal = 65;
         std::cerr << "[line " << line_num << "] Error: Unexpected character: " << ch
